e9a5ac5ec88e8c84aaa8ef417423e2e5440c5530
Yosip Mike Colin <colinparrado@gmail.com>
2024-09-17 19:34:30 -0500

    Route to obtain places

--- src/controllers/place.controller.js
+++ src/controllers/place.controller.js
@@ -1,15 +1,20 @@
 import Place from "../models/place.model.js";
 
-export const getPlaces = (req, res) => {
-  console.log("Obteniendo plazas");
-  res.json({ message: "Obteniendo creadas" });
+export const getPlaces = async (_req, res) => {
+  try {
+    const places = await Place.findAll();
+    res.status(200).json(places);
+  } catch (error) {
+    res.status(500).json({ message: `Error ${error}` });
+  }
 };
 
 export const postPlace = async (req, res) => {
   try {
     const data = req.body;
-    await Place.create(data);
-    res.status(201).json({ message: "Place created successfully" });
+    const response = await Place.create(data);
+    const { place } = response.dataValues;
+    res.status(201).json({ message: `Place ${place} created successfully` });
   } catch (error) {
     res.status(500).json({ message: `Error ${error}` });
   }
